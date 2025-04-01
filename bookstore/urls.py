600f6e0a319ca4584f4782df67d01d3fef5d03c5
victoormga <vvvvvggggaaaa11@gmail.com>
2025-04-01 18:53:54 +0200

    changes added.

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
 ]
