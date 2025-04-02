5021fc790a7489a298e3fccdd4e76150f3ab66d1
victoormga <vvvvvggggaaaa11@gmail.com>
2025-04-02 10:13:31 +0200

    Sign Up.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('signup/', views.sign_up, name='signup'),
+    path('login/', views.log_in, name='login'),
+    path('logout/', views.log_out, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import CustomUserCreationForm
+from .models import Customuser
+from django.contrib.auth.decorators import login_required
 
-def signup(request):
-    return render(request, 'accounts/signup.html')
-
-def login(request):
+# Log In view
+def log_in(request):
     return render(request, 'accounts/login.html')
 
-def logout(request):
-    return render(request, 'pages/home.html')
+# Sign Up view
+def sign_up(request):
+    template_data = {'title' : 'Sign Up'}
+
+    if request.method == 'GET':
+        template_data['form'] = CustomUserCreationForm()
+        return render(request, 'accounts/signup.html', template_data)
+    
+    elif request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('log_in')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html', template_data)
+
+# Log Out view
+@login_required
+def log_out(request):
+    logout(request)
+    return redirect('home')
