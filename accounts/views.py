3ee99b5489d8512c1d3513e1f3e0d3b63d84fcc4
victoormga <vvvvvggggaaaa11@gmail.com>
2025-04-02 11:41:55 +0200

    Project paused.

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .forms import CustomUserCreationForm
-from .models import Customuser
 from django.contrib.auth.decorators import login_required
-
-# Log In view
-def log_in(request):
-    return render(request, 'accounts/login.html')
+from django.contrib.auth.forms import AuthenticationForm
 
 # Sign Up view
 def sign_up(request):
@@ -20,10 +16,29 @@ def sign_up(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('log_in')
+            return redirect('login')
         else:
             template_data['form'] = form
             return render(request, 'accounts/signup.html', template_data)
+
+# Log In view
+def log_in(request):
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username = request.POST['username'], password = request.POST['password']
+        )
+        if user is None:
+            return render(request, 'accounts/login.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
 
 # Log Out view
 @login_required
