2ec56708d91b55f5d0b0e9ba5cd601beaaddd33d
victoormga <vvvvvggggaaaa11@gmail.com>
2025-04-07 11:22:02 +0200

    Account's funcionality working (watch out with wirting the worng values in the login form).

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from accounts import views
 
 urlpatterns = [
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static'] # Agrega la carpeta static
+# STATICFILES_DIRS = [BASE_DIR / 'static'] # Agrega la carpeta static
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
