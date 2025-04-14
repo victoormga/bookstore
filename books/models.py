09119ec546921e313b487e8a17035378ed2603de
victoormga <vvvvvggggaaaa11@gmail.com>
2025-04-14 10:55:09 +0200

    Added some models.

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from authors.models import Author
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genre = models.ManyToManyField(Genre)
+    editor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    published_date = models.DateField()
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
