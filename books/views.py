a52e51e82b94409114a6976d283400121775029c
victoormga <vvvvvggggaaaa11@gmail.com>
2025-04-23 09:11:08 +0200

    Added somethings.

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from django.core.paginator import Paginator
 
-# Create your views here.
+# Vista para listar libros
+def book_list(request):
+    books = Book.objects.all().order_by('-published_date')
+    paginator = Paginator(books, 5)  # 5 libros por página
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'books/book_list.html', {'page_obj': page_obj})
+
+
+# Vista para ver detalle de un libro
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/book_details.html', {'book': book})
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     path('accounts/', include('accounts.urls')),
+    path('books/', include('books.urls')),
 ]
