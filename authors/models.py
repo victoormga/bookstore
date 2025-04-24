a74d325199f0b2b61815b01b493417f5c294cce8
victoormga <vvvvvggggaaaa11@gmail.com>
2025-04-24 13:30:10 +0200

    More.

--- authors/models.py
+++ authors/models.py
@@ -3,4 +3,3 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
     biography = models.TextField()
-
--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import AuthorForm
 
-# Create your views here.
+@login_required
+def add_author(request):
+    if not request.user.is_superuser:
+        messages.error(request, 'No tienes permiso para añadir autores.')
+        return redirect('book_list')  # Puedes cambiar esto por donde quieras redirigir
+
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            author = form.save() 
+            messages.success(request, f'Se añadió el autor "{author.name}" correctamente.')
+            return redirect('author_list')  
+    else:
+        form = AuthorForm()
+
+    return render(request, 'authors/add_author.html', {'form': form})
